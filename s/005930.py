891fdbaf50f934366b040b7401c6bb03502e0e8a
kangsik10 <kstobit@naver.com>
2023-09-17 23:09:21 +0900

    at home on Sunday

--- s/005930.py
+++ s/005930.py
@@ -1,12 +1,28 @@
-import requests
-from bs4 import BeautifulSoup
-key = input("회사명을 입력하시오 : ")
-response = requests.get(f"https://search.naver.com/search.naver?where=news&sm=tab_jum&query={key}")
+import FinanceDataReader as fdr
+import pandas as pd
+df_krx = fdr.StockListing('KRX')
+df_krx
 
-html = response.text
-soup = BeautifulSoup(html, 'html.parser')
-links = soup.select(".news_tit")
-for link in links:
-    title = link.text
-    url = link.attrs['href']
-    print(title, url)
+# import FinanceDataReader as fdr
+# import pandas as pd
+# df_krx =fdr.StockListing('KRX').iloc[:, :3]
+# stocks = ["005930"]
+# df1 = pd.DataFrame()
+# for i in stocks:
+#     df = dfr.DataReader(i, '2023-08-01','2023-08-31')
+#     df["Symbol"] = i
+#     df1 = pd.concat([df1, df])
+# df1.reset_index().merge(df_krx, how="left")
+
+# import requests
+# from bs4 import BeautifulSoup
+# key = input("회사명을 입력하시오 : ")
+# response = requests.get(f"https://search.naver.com/search.naver?where=news&sm=tab_jum&query={key}")
+
+# html = response.text
+# soup = BeautifulSoup(html, 'html.parser')
+# links = soup.select(".news_tit")
+# for link in links:
+#     title = link.text
+#     url = link.attrs['href']
+#     print(title, url)
---
+++ s/news.py
@@ -0,0 +1,54 @@
+from bs4 import BeautifulSoup
+import urllib.request
+from urllib.parse import quote
+import pandas as pd
+
+
+def get_news(query, page_num=10):            
+    news_df = pd.DataFrame(columns=("Title", "Link", "Press", "Datetime", "Article"))
+    idx = 0
+
+    url_query = quote(query)
+    url = "https://search.naver.com/search.naver?where=news&ie=utf8&sm=nws_hty&query="+url_query
+
+    for _ in range(0, page_num):
+        search_url = urllib.request.urlopen(url).read()
+        soup = BeautifulSoup(search_url,'html.parser')
+        links = soup.find_all('dd',{'class':'txt_inline'})
+
+        for link in links:
+            press = link.find('span',{'class':'_sp_each_source'}).get_text()
+            news_url = link.find('a').get('href')
+           
+            if (news_url=='#'):
+                continue
+            else:
+                news_link = urllib.request.urlopen(news_url).read()
+                news_html = BeautifulSoup(news_link, 'html.parser')
+                
+                try:
+                    title = news_html.find('h3',{'id':'articleTitle'}).get_text()
+                    datetime = news_html.find('span',{'class':'t11'}).get_text()
+                    article = news_html.find('div',{'id':'articleBodyContents'}).get_text()
+                    article = article.replace("// flash 오류를 우회하기 위한 함수 추가", "")
+                    article = article.replace("function _flash_removeCallback() {}", "")
+                    article = article.replace("\n","")
+                    article = article.replace("\t", "")
+        
+                    news_df.loc[idx] = [title, news_url, press, datetime, article]
+                    idx +=1    
+                    print("#", end="")
+                except:
+                    continue
+
+        try:
+            next = soup.find('a',{'class':'next'}).get('href')
+            url = "https://search.naver.com/search.naver"+next
+        except:
+            break
+
+    return news_df
+
+query=input('검색 질의: ')
+news_df = get_news(query, 10)
+print('Done')
