c011bbcda82e189be469de824eaf60ed8fca28b4
kangsik10 <kstobit@naver.com>
2023-09-19 14:57:57 +0900

    from the bottom

--- s/005930.py
+++ s/005930.py
@@ -1,8 +1,18 @@
-import BeautifulSoup
+from bs4 import BeautifulSoup
 import requests
 
 write.open('stock.csv')
 
+headers = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"
+
+BeautifulSoup
+bs = https://finance.naver.com/item/sise.naver?code=005930
+bs.response
+
+
+for i in b = select(table .type2 > tr > td):
+
+print(b)
 
 
 
