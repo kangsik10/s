5f2f45cdf23dc15bc99ec982abdae12bd973b5e8
ks <kstobit@naver.com>
2023-11-09 17:04:40 +0900

    초소형가격예측모델 오류수정

--- fun_reputation/twitter.py
+++ fun_reputation/twitter.py
@@ -1,11 +1,26 @@
-import requests
-import urllib.parse
 
-print(urllib.parse.quote("지드래곤"))
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+import time
 
-url = ''
-response = requests.get(url, headers=headers)
-print(response.json())
+options=Options()
+options.add_experimental_option('detach', True) #브라우저 바로꺼짐 방지
 
-for x in response.json()['data']['search_timeline'][]
+driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+driver.get('https://twitter.com/i/flow/login')
+driver.implicitly_wait(10)
 
+driver.find_element(By.CSS_SELECTOR, "input.r-30o5oe").send_keys("dante@fins.ai")
+time.sleep(1)
+driver.find_elements(By.CSS_SELECTOR, ".css-901oao.css-16my406.r-poiln3")[8].click()
+time.sleep(1)
+driver.find_element(By.CSS_SELECTOR, "input.r-30o5oe").send_keys("01063108802")
+time.sleep(1)
+driver.find_elements(By.CSS_SELECTOR, "span.css-901oao")[6].click()
+time.sleep(1)
+driver.find_elements(By.CSS_SELECTOR, "input.r-30o5oe")[1].send_keys("abcd!@#$5678")
+time.sleep(1)
+driver.find_elements(By.CSS_SELECTOR, ".css-901oao.css-16my406.r-poiln3")[13].click()
+time.sleep(1)
